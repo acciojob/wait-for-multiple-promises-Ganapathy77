146dc9d26ae087e656739462873d3123832a8b2c
Ganapathy77 <106886401+Ganapathy77@users.noreply.github.com>
2023-03-26 17:11:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
 //your JS code here. If required.
-// create three promises that resolve after a random time between 1 and 3 seconds
-  const promise1 = new Promise((resolve) => setTimeout(() => resolve('Promise 1'), Math.random() * 2000 + 1000));
-  const promise2 = new Promise((resolve) => setTimeout(() => resolve('Promise 2'), Math.random() * 2000 + 1000));
-  const promise3 = new Promise((resolve) => setTimeout(() => resolve('Promise 3'), Math.random() * 2000 + 1000));
+const promise1 = new Promise((resolve)=>{
+	let time =Number( Math.random() * 2000 + 1000 )
+	console.log(time)
+	setTimeout(()=>{
+		resolve(time)
+	},time)
+})
+const promise2 = new Promise((resolve)=>{
+	let time = Math.random() * 2000 + 1000
+	setTimeout(()=>{
+		resolve(time)
+	},time)
+})
+const promise3 = new Promise((resolve,reject)=>{
+	let time = Math.random() * 2000 + 1000
+	setTimeout(()=>{
+		resolve(time)
+	},time)
+})
 
-  Promise.all([promise1, promise2, promise3])
-    .then((results) => {
-      const loadingRow = document.getElementById('loading');
-      loadingRow.parentNode.removeChild(loadingRow);
-      const tableBody = document.querySelector('tbody');
-      results.forEach((result, index) => {
-        const row = tableBody.insertRow(index);
-        const promiseCell = row.insertCell(0);
-        promiseCell.textContent = result;
-        const timeTakenCell = row.insertCell(1);
-        timeTakenCell.textContent = ((index + 1) * 1000 + Math.random() * 2000) / 1000;
-      });
-      const totalRow = tableBody.insertRow();
-      const totalCell = totalRow.insertCell(0);
-      totalCell.textContent = 'Total';
-      const totalTimeTakenCell = totalRow.insertCell(1);
-      totalTimeTakenCell.textContent = (Date.now() - performance.timing.navigationStart) / 1000;
-    });
-       
+Promise.all([promise1,promise2,promise3]).then((results)=>{
+	const loading = document.querySelector('#loading');
+	loading.innerHTML = "";
+
+	const tableBody = document.querySelector('tbody');
+	let totalTime = 0;
+	results.forEach((result,index)=>{
+		const row = tableBody.insertRow(index);
+		const cell = row.insertCell(0)
+		cell.textContent = `Promise ${index+1}`;
+		const timeTakenCell = row.insertCell(1);
+		timeTakenCell.textContent =Math.round(result/1000);
+		totalTime += result;
+	})
+
+	const totalRow = tableBody.insertRow();
+	const totalCell = totalRow.insertCell(0);
+	totalCell.textContent = 'Total';
+	const totalTimeTakenCell = totalRow.insertCell(1);
+	totalTimeTakenCell.textContent += (totalTime/3000).toFixed(4)
+})
