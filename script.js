3364d475924c4f018af5ba6a2ecf45d8d4efee79
Ganapathy77 <ganapathy14777@gmail.com>
2023-03-26 16:57:25 +0530

    Completed Coding

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 //your JS code here. If required.
 const promise1 = new Promise((resolve)=>{
-	let time = Math.random*2000+1000 
+	let time =Number( Math.random() * 2000 + 1000 )
+	console.log(time)
 	setTimeout(()=>{
 		resolve(time)
 	},time)
 })
 const promise2 = new Promise((resolve)=>{
-	let time = Math.random*2000+1000 
+	let time = Math.random() * 2000 + 1000
 	setTimeout(()=>{
 		resolve(time)
 	},time)
 })
 const promise3 = new Promise((resolve,reject)=>{
-	let time = Math.random*2000+1000 
+	let time = Math.random() * 2000 + 1000
 	setTimeout(()=>{
 		resolve(time)
 	},time)
@@ -23,9 +24,19 @@ Promise.all([promise1,promise2,promise3]).then((results)=>{
 	loading.innerHTML = "";
 
 	const tableBody = document.querySelector('tbody');
+	let totalTime = 0;
 	results.forEach((result,index)=>{
 		const row = tableBody.insertRow(index);
-		const cell = row.insertRow(0);
-		cell.textContent = `Promise ${index+1}`
+		const cell = row.insertCell(0)
+		cell.textContent = `Promise ${index+1}`;
+		const timeTakenCell = row.insertCell(1);
+		timeTakenCell.textContent =Math.round(result/1000);
+		totalTime += result;
 	})
+
+	const totalRow = tableBody.insertRow();
+	const totalCell = totalRow.insertCell(0);
+	totalCell.textContent = 'Total';
+	const totalTimeTakenCell = totalRow.insertCell(1);
+	totalTimeTakenCell.textContent += (totalTime/3000).toFixed(4)
 })
